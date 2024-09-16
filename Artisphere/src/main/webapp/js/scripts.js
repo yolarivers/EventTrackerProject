1d322e43cdf24ecc79830738016bb9c16f847da9
Yola Rivers <yolarivers@gmail.com>
2024-09-16 01:22:24 -0500

    Modified css, js and html files.

--- Artisphere/src/main/webapp/js/scripts.js
+++ Artisphere/src/main/webapp/js/scripts.js
@@ -1,3 +1,13 @@
-/**
- * 
- */
+
+document.getElementById('allDay').addEventListener('change', function() {
+    const startTime = document.getElementById('startTime');
+    const endTime = document.getElementById('endTime');
+    
+    if (this.checked) {
+        startTime.disabled = true;
+        endTime.disabled = true;
+    } else {
+        startTime.disabled = false;
+        endTime.disabled = false;
+    }
+});
